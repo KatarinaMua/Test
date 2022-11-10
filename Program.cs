cab735a0d481925e86088b8959a09311e9bb72d2
Ekaterina Tikhomirova <95663361+KatarinaMua@users.noreply.github.com>
2022-11-10 21:36:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿string[] arr1 = new string[6] {"hello", "2", "world", "computer sciense", "Russia", "Pur"};
+string[] arr2 = new string[arr1.Length];
+int a = 0;
+CreateArr(arr1, arr2);
+PrintArr(arr2);
+void CreateArr(string[] arr1, string[] arr2)
+{
+    int a = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+    if(arr1[i].Length <= 3)
+        {
+        arr2[a] = arr1[i];
+        a++;
+        }
+    }
+}
+void PrintArr(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
